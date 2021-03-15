8d27aa2e7156e225b0f0b4127277827af858c486
QSP <QSP@DESKTOP-EVK9GMC>
2021-03-15 17:37:04 +0530

    LaunchingBrowsers

---
+++ src/basics/Test0.java
@@ -0,0 +1,15 @@
+package basics;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Test0 {
+
+	public static void main(String[] args) {
+	//ChromeDriver driver=new ChromeDriver();
+		
+		FirefoxDriver driver=new FirefoxDriver();
+
+	}
+
+}
